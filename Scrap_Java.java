fe39f094012978dc0352d7e4ae5c19cf77a8f379
Mauro Caredda <mauro.dev88@gmail.com>
2020-12-30 22:12:38 +0100

    Update Scrap_Java.java

--- Scrap_Java.java
+++ Scrap_Java.java
@@ -27,3 +27,80 @@
 
 			}
 		});
+
+
+#############################
+package com.company;
+
+import okhttp3.*;
+import okio.Utf8;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+                Request request1 = new Request.Builder()
+                .url("URL1")
+                .build();
+
+        Request request2 = new Request.Builder()
+                .url("URL2")
+                .build();
+
+        client.newCall(request1).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()){
+                    String resp = response.body().string();
+                    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder db = null;
+                    try {
+                        db = dbf.newDocumentBuilder();
+                    } catch (ParserConfigurationException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        XPath xpath =XPathFactory.newInstance().newXPath();
+                        org.w3c.dom.Document xmlDoc = db.parse(new InputSource(new ByteArrayInputStream(resp.getBytes("UTF-8"))));
+                        XPathExpression name = xpath.compile("XPATH");
+                        NodeList nodeList = (NodeList) name.evaluate(xmlDoc, XPathConstants.NODESET);
+                        for (int i = 0; i < nodeList.getLength(); i++) {
+                            Node node = nodeList.item(i);
+                            System.out.println(node.getNodeName() + ": " + node.getTextContent());
+                        }
+                    } catch (SAXException | XPathExpressionException e) {
+                        e.printStackTrace();
+                    }
+
+                    client.newCall(request2).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            System.out.println(response.body().string());
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
